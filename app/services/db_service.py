8434c442a2ab721d40d0e695272797a1ee618045
himanshumishra9911 <himanshumishra9911@gmail.com>
2026-02-25 12:56:05 +0530

    Implement save_scan function in db_service.py

---
+++ app/services/db_service.py
@@ -0,0 +1,12 @@
+from app.core.database import db
+from datetime import datetime
+
+
+async def save_scan(scan_type, content, verdict, risk_score):
+    await db.scam_checks.insert_one({
+        "type": scan_type,
+        "content": content[:2000],
+        "verdict": verdict,
+        "risk_score": risk_score,
+        "created_at": datetime.utcnow()
+    })
