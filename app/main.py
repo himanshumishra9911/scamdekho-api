9c5387f3538b1a95f3cf9e59fd71771048198058
himanshumishra9911 <himanshumishra9911@gmail.com>
2026-04-05 21:17:25 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,8 +6,15 @@ from app.api.analytics import router as analytics_router
 from app.api.feedback import router as feedback_router
 from app.services.cache_service import setup_cache_ttl_index
 from app.services.website_screenshot import setup_screenshot_cache_index
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
 
 app = FastAPI(title="ScamDekho API")
+app.state.limiter = Limiter(key_func=get_remote_address)
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
 
 # ================= CORS CONFIG =================
 origins = [
